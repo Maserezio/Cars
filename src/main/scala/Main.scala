71d03e3115388057769ba1c78534b127ad4e03ff
Artur <49900277+Maserezio@users.noreply.github.com>
2021-11-24 17:55:18 +0300

    parsed table with red outline template to json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,45 @@
-
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
 
 import scala.collection.mutable.ListBuffer
-import scala.util.matching.Regex
 
 object Main {
-  //case class Car( id:Int, name:String, founded:Int, founder:List[String], country:String, website:String)
 
   def main(args: Array[String]) = {
     val browser = JsoupBrowser()
-    val page = browser.get("https://www.carlogos.org/car-brands/honda-logo.html")
+    val page = browser.get("https://www.carlogos.org/car-brands/bmw-logo.html")
 
     val info = ListBuffer.empty ++= (page >> elementList(".brand-overview p").map(_ >> allText("p")))
       .filter(el => el.contains("Founded") || el.contains("Founder") || el.contains("Headquarters") || el.contains("Official Site"))
 
-    val fin = info.toList
-    for (x <- info) {
-      println(x.substring(x.indexOf(':') + 2))
-    }
+    val founded = getFounded(info(0))
+    val founder = getFounder(info(1))
+    val head = getHeadquarters(info(2))
+    val site = getSite(info(3))
 
-    println("")
 
-    val str = "Minato, Tokyo, Japan"
-    val reg: Regex = "(, )([\\s\\w]+)$".r
+    val id = 1
+    val name = "Honda"
+    val specialization = "Mass-Market Cars"
 
+    val json = ("id" -> id)~
+      ("name" ->name)~
+      ("specialization" -> specialization)~
+      ("founded" -> founded)~
+      ("founder" -> founder)~
+      ("country" -> head)~
+      ("site" -> site)
+    println(prettyRender(json))
     
-
-    for (patternMatch <- reg.findAllMatchIn(str))
-      println(patternMatch.group(2))
   }
+
+  private def getFounded(str: String) = str.substring(str.indexOf(":")+2)
+  private def getFounder(str: String) = str.substring(str.indexOf(':')+2).split("(, )|( / )").toList
+  private def getHeadquarters(str: String) = "([\\w\\s.]+)$".r.findAllMatchIn(str).mkString("").substring(1)
+  private def getSite(str: String) = str.substring(str.indexOf(":")+2)
 }
 
