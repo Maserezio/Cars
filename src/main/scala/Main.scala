41e2883e359d79125aab61767f3e4336651b934b
Artur <49900277+Maserezio@users.noreply.github.com>
2021-11-25 06:13:54 +0300

    added array of strings to parse all site

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,27 +3,54 @@ import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
-import net.ruippeixotog.scalascraper.browser.JsoupBrowser.JsoupElement
 
 import scala.collection.mutable.ListBuffer
 
 object Main {
 
-  def main(args: Array[String]) = {
-    val browser = JsoupBrowser()
-    val page = browser.get("https://www.carlogos.org/car-brands/")
+  val pages = List(
+    "https://www.carlogos.org/car-brands/",
+    "https://www.carlogos.org/car-brands/page-2.html",
+    "https://www.carlogos.org/car-brands/page-3.html",
+    "https://www.carlogos.org/car-brands/page-4.html",
+    "https://www.carlogos.org/car-brands/page-5.html",
+    "https://www.carlogos.org/car-brands/page-6.html",
+    "https://www.carlogos.org/car-brands/page-7.html",
+    "https://www.carlogos.org/car-brands/page-8.html",
+  )
 
+  def main(args: Array[String]) = {
+
+    val listOfPages = getListOfCars(pages(0))
+    listOfPages ++= getListOfCars(pages(1))
+    listOfPages ++= getListOfCars(pages(2))
+    listOfPages ++= getListOfCars(pages(3))
+    listOfPages ++= getListOfCars(pages(4))
+    listOfPages ++= getListOfCars(pages(5))
+    listOfPages ++= getListOfCars(pages(6))
+    listOfPages ++= getListOfCars(pages(7))
+
+  val jsonList = ListBuffer[String]().empty
+    for (i <- listOfPages.indices) {
+      jsonList += parse(listOfPages(i))
+    }
+  }
+
+
+  def getListOfCars(_page: String) = {
+    val page = JsoupBrowser().get(_page)
     val names = ListBuffer.empty ++= (page >> elementList(".logo-list li a") >> attr("alt")("img"))
       .map(i => i.substring(0, i.length - 5))
 
     val links = ListBuffer.empty ++= (page >> elementList(".logo-list li") >> attr("href")("a"))
 
     val spans = ListBuffer.empty ++= (page >> elementList(".logo-list li a span").map(_ >> allText("span")))
+
     val (evenWithIndex, oddWithIndex) = spans.zipWithIndex.partition(_._2 % 2 == 0)
     val spec = evenWithIndex.map(_._1)
     val founded = oddWithIndex.map(_._1)
-    val listOfPages = ListBuffer[List[Serializable]]().empty
 
+    val listOfPages = ListBuffer[List[Serializable]]().empty
     for (i <- names.indices) {
       if (founded(i).contains("Present") || founded(i).contains("present")) {
         listOfPages += Array(
@@ -33,17 +60,12 @@ object Main {
         ).toList
       }
     }
-
-    for (i <- listOfPages.indices) {
-      parse(listOfPages(i))
-    }
-
-
+    listOfPages
   }
 
-  private def parse(preview: List[Serializable]): Unit = {
-    val browser = JsoupBrowser()
-    val page = browser.get("https://www.carlogos.org/car-brands" + s"${preview(2)}")
+  private def parse(preview: List[Serializable]): String = {
+
+    val page = JsoupBrowser().get("https://www.carlogos.org/car-brands" + s"${preview(2)}")
 
     if (page >> allText(".brand-overview p") != "") {
       val info = ListBuffer.empty ++= (page >> elementList(".brand-overview p").map(_ >> allText("p")))
@@ -61,7 +83,8 @@ object Main {
         ("founder" -> founder) ~
         ("country" -> head) ~
         ("site" -> site)
-      println(prettyRender(json))
+
+      prettyRender(json)
     }
     else {
       val raw = (page >> elementList(".content table tbody tr").map(_ >> elementList("td"))).drop(1)
@@ -77,7 +100,6 @@ object Main {
           .replaceAll("(JsoupElement)|(<\\/a>)|(\\(<td>)|(\\(<td.+1\">)|(<\\/td>\\))|(<a.+\">)|(<\\/?p>)|(<\\/?strong>)|(&(nbsp|amp);)|",
             "")
       }
-
       val finalList = parsedRaw.filter(el => el.contains("Founded:") || el.contains("Founder:") || el.contains("Headquarters:") || el.contains("Official Site:"))
 
       val founded = if (finalList.find(_.contains("Founded:")) != None) getFounded(finalList.find(_.contains("Founded:")).get) else ""
@@ -92,13 +114,13 @@ object Main {
         ("country" -> head) ~
         ("site" -> site)
 
-      println(prettyRender(json))
+      prettyRender(json)
     }
   }
 
   private def getFounded(str: String) = "\\d{4}".r.findFirstMatchIn(str).mkString("")
 
-  private def getFounder(str: String) = str.substring(str.indexOf(':')+2).split("(, )|( / )|( · )").toList
+  private def getFounder(str: String) = str.substring(str.indexOf(':') + 2).split("(, )|( / )|( · )").toList
 
   private def getHeadquarters(str: String) = "([\\w\\s.]+)$".r.findAllMatchIn(str).mkString("").substring(1)
 
