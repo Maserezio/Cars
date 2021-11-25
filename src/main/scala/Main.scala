347abf079cbe6284f8af83bf7b441113859e69f2
Artur <49900277+Maserezio@users.noreply.github.com>
2021-11-25 05:29:05 +0300

    made parsing of all types to json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,8 @@ import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser.JsoupElement
+
 import scala.collection.mutable.ListBuffer
 
 object Main {
@@ -17,16 +19,14 @@ object Main {
     val links = ListBuffer.empty ++= (page >> elementList(".logo-list li") >> attr("href")("a"))
 
     val spans = ListBuffer.empty ++= (page >> elementList(".logo-list li a span").map(_ >> allText("span")))
-
     val (evenWithIndex, oddWithIndex) = spans.zipWithIndex.partition(_._2 % 2 == 0)
     val spec = evenWithIndex.map(_._1)
     val founded = oddWithIndex.map(_._1)
-
-    val kek = ListBuffer[List[Serializable]]().empty
+    val listOfPages = ListBuffer[List[Serializable]]().empty
 
     for (i <- names.indices) {
       if (founded(i).contains("Present") || founded(i).contains("present")) {
-        kek += Array(
+        listOfPages += Array(
           names(i),
           spec(i).split("(, )").toList,
           links(i).substring(11)
@@ -34,36 +34,71 @@ object Main {
       }
     }
 
-    kek.toList
+    for (i <- listOfPages.indices) {
+      parse(listOfPages(i))
+    }
 
-    parse(kek(0))
+
   }
 
   private def parse(preview: List[Serializable]): Unit = {
     val browser = JsoupBrowser()
     val page = browser.get("https://www.carlogos.org/car-brands" + s"${preview(2)}")
 
-    val info = ListBuffer.empty ++= (page >> elementList(".brand-overview p").map(_ >> allText("p")))
-      .filter(el => el.contains("Founded") || el.contains("Founder") || el.contains("Headquarters") || el.contains("Official Site"))
+    if (page >> allText(".brand-overview p") != "") {
+      val info = ListBuffer.empty ++= (page >> elementList(".brand-overview p").map(_ >> allText("p")))
+        .filter(el => el.contains("Founded") || el.contains("Founder") || el.contains("Headquarters") || el.contains("Official Site"))
 
-    val founded = getFounded(info.head)
-    val founder = getFounder(info(1))
-    val head = getHeadquarters(info(2))
-    val site = getSite(info(3))
+      val founded = if (info.find(_.contains("Founded:")) != None) getFounded(info.find(_.contains("Founded:")).get) else ""
+      val founder = if (info.find(_.contains("Founder:")) != None) getFounder(info.find(_.contains("Founder: ")).get) else List.empty
+      val head = if (info.find(_.contains("Headquarters:")) != None) getHeadquarters(info.find(_.contains("Headquarters:")).get) else ""
+      val site = if (info.find(_.contains("Official Site:")) != None) getSite(info.find(_.contains("Official Site:")).get) else ""
 
 
-    val json = ("name" -> preview.head.toString) ~
-      ("specialization" -> preview(1).asInstanceOf[List[String]]) ~
-      ("founded" -> founded) ~
-      ("founder" -> founder) ~
-      ("country" -> head) ~
-      ("site" -> site)
-    println(prettyRender(json))
+      val json = ("name" -> preview.head.toString) ~
+        ("specialization" -> preview(1).asInstanceOf[List[String]]) ~
+        ("founded" -> founded) ~
+        ("founder" -> founder) ~
+        ("country" -> head) ~
+        ("site" -> site)
+      println(prettyRender(json))
+    }
+    else {
+      val raw = (page >> elementList(".content table tbody tr").map(_ >> elementList("td"))).drop(1)
+      val parsedRaw = ListBuffer[String]().empty
+      for (i <- raw) {
+        parsedRaw += i
+          .mkString(": ")
+          .replace("<br>", ", ")
+          .replaceAll("(Production)|(MarfinalListts)", "Founded")
+          .replaceAll("(Designer)|(Predecessor)", "Founder")
+          .replaceAll("(Country)|(Assembly)", "Headquarters")
+          .replace("Website", "Official Site")
+          .replaceAll("(JsoupElement)|(<\\/a>)|(\\(<td>)|(\\(<td.+1\">)|(<\\/td>\\))|(<a.+\">)|(<\\/?p>)|(<\\/?strong>)|(&(nbsp|amp);)|",
+            "")
+      }
+
+      val finalList = parsedRaw.filter(el => el.contains("Founded:") || el.contains("Founder:") || el.contains("Headquarters:") || el.contains("Official Site:"))
+
+      val founded = if (finalList.find(_.contains("Founded:")) != None) getFounded(finalList.find(_.contains("Founded:")).get) else ""
+      val founder = if (finalList.find(_.contains("Founder:")) != None) getFounder(finalList.find(_.contains("Founder:")).get) else List.empty
+      val head = if (finalList.find(_.contains("Headquarters:")) != None) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else ""
+      val site = if (finalList.find(_.contains("Official Site:")) != None) getSite(finalList.find(_.contains("Official Site:")).get) else ""
+
+      val json = ("name" -> preview.head.toString) ~
+        ("specialization" -> preview(1).asInstanceOf[List[String]]) ~
+        ("founded" -> founded) ~
+        ("founder" -> founder) ~
+        ("country" -> head) ~
+        ("site" -> site)
+
+      println(prettyRender(json))
+    }
   }
 
-  private def getFounded(str: String) = str.substring(str.indexOf(":") + 2)
+  private def getFounded(str: String) = "\\d{4}".r.findFirstMatchIn(str).mkString("")
 
-  private def getFounder(str: String) = str.substring(str.indexOf(':') + 2).split("(, )|( / )").toList
+  private def getFounder(str: String) = str.substring(str.indexOf(':')+2).split("(, )|( / )|( · )").toList
 
   private def getHeadquarters(str: String) = "([\\w\\s.]+)$".r.findAllMatchIn(str).mkString("").substring(1)
 
