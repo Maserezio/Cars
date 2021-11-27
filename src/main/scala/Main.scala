19be8dc9b48d09ff671c251ce6ec8e50e3f4f20b
Artur <49900277+Maserezio@users.noreply.github.com>
2021-11-27 19:26:21 +0300

    Changed writing to separate file for each json using case classes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,9 +5,12 @@ import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 
 import java.io.{File, PrintWriter}
+import java.nio.file.Files
 import scala.collection.mutable.ListBuffer
 
 object Main {
+
+  case class Car(name: String, spec: List[String], founded: String, founder: List[String], country: String, site: String)
 
   val pages = List(
     "https://www.carlogos.org/car-brands/",
@@ -27,20 +30,12 @@ object Main {
       listOfCars ++= getListOfCars(pages(i))
 
     try {
-      val pw = new PrintWriter(new File("JSON.json"))
-      pw.write("[")
-      for (i <- listOfCars.indices) {
-        if (i != listOfCars.size - 1)
-          pw.write(parse(listOfCars(i)) + ",\n")
-        else
-          pw.write(parse(listOfCars(i)))
-      }
-      pw.write("]")
-      pw.close
+      for(i<-listOfCars.indices)
+      writeToFile(listOfCars(i).head.toString, parse(listOfCars(i)))
     } catch {
       case e: Exception => println("Error: %s".format(e.getMessage))
     } finally {
-      println("Writing to file finished!")
+      println("Writing to JSON files finished!")
     }
 
   }
@@ -78,17 +73,20 @@ object Main {
     val page = JsoupBrowser().get("https://www.carlogos.org/car-brands" + s"${preview(2)}")
 
     if (page >> allText(".brand-overview p") != "") {
-      val info = ListBuffer.empty ++= (page >> elementList(".brand-overview p").map(_ >> allText("p")))
+      val finalList = ListBuffer.empty ++= (page >> elementList(".brand-overview p").map(_ >> allText("p")))
         .filter(el => el.contains("Founded") || el.contains("Founder") || el.contains("Headquarters") || el.contains("Official Site"))
 
-      formJSON(
+      val car = Car(
         preview.head.toString,
         preview(1).asInstanceOf[List[String]],
-        if (info.find(_.contains("Founded:")) != None) getFounded(info.find(_.contains("Founded:")).get) else "",
-        if (info.find(_.contains("Founder:")) != None) getFounder(info.find(_.contains("Founder: ")).get) else List.empty,
-        if (info.find(_.contains("Headquarters:")) != None) getHeadquarters(info.find(_.contains("Headquarters:")).get) else "",
-        if (info.find(_.contains("Official Site:")) != None) getSite(info.find(_.contains("Official Site:")).get) else ""
+        if (finalList.find(_.contains("Founded:")) != None) getFounded(finalList.find(_.contains("Founded:")).get) else "",
+        if (finalList.find(_.contains("Founder:")) != None) getFounder(finalList.find(_.contains("Founder: ")).get) else List.empty,
+        if (finalList.find(_.contains("Headquarters:")) != None) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else "",
+        if (finalList.find(_.contains("Official Site:")) != None) getSite(finalList.find(_.contains("Official Site:")).get) else ""
       )
+
+      formJSON(car.name, car.spec, car.founded, car.founder, car.country, car.site)
+
     }
     else {
       val raw = (page >> elementList(".content table tbody tr").map(_ >> elementList("td"))).drop(1)
@@ -106,7 +104,7 @@ object Main {
       }
       val finalList = parsedRaw.filter(el => el.contains("Founded:") || el.contains("Founder:") || el.contains("Headquarters:") || el.contains("Official Site:"))
 
-      formJSON(
+      val car = Car(
         preview.head.toString,
         preview(1).asInstanceOf[List[String]],
         if (finalList.find(_.contains("Founded:")) != None) getFounded(finalList.find(_.contains("Founded:")).get) else "",
@@ -114,6 +112,9 @@ object Main {
         if (finalList.find(_.contains("Headquarters:")) != None) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else "",
         if (finalList.find(_.contains("Official Site:")) != None) getSite(finalList.find(_.contains("Official Site:")).get) else ""
       )
+
+      formJSON(car.name, car.spec, car.founded, car.founder, car.country, car.site)
+
     }
   }
 
@@ -125,6 +126,13 @@ object Main {
       ("country" -> country) ~
       ("site" -> site)
   )
+
+  private def writeToFile(name: String, json: String): Unit = {
+    val dir = new File("D:\\Uni\\NoSQL\\Cars\\JSON\\").mkdir()
+    val pw = new PrintWriter(new File("D:\\Uni\\NoSQL\\Cars\\JSON\\" + s"${name}" + ".json"))
+    pw.write(json)
+    pw.close()
+  }
 
   private def getFounded(str: String) = "\\d{4}".r.findFirstMatchIn(str).mkString("")
 
