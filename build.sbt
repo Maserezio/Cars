49509decbb28d3522c24957b82906afb777d1c56
Artur <49900277+Maserezio@users.noreply.github.com>
2021-11-30 02:10:43 +0300

    wroted data to MongoDB and wrote queries

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ scalaVersion := "2.13.7"
 
 libraryDependencies += "net.ruippeixotog" %% "scala-scraper" % "2.2.1"
 libraryDependencies += "net.liftweb" %% "lift-json" % "3.5.0"
+libraryDependencies += "org.mongodb.scala" %% "mongo-scala-driver" % "4.4.0"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.7"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,22 @@
+import ch.qos.logback.classic.{Level, LoggerContext}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
+import Helpers._
 
 import java.io.{File, PrintWriter}
-import java.nio.file.Files
 import scala.collection.mutable.ListBuffer
+import org.mongodb.scala._
+import org.mongodb.scala.bson.BsonDocument
+import org.mongodb.scala.model.Filters.{and, equal, exists, lt, lte, or, size}
+import org.mongodb.scala.model.Indexes.descending
+import org.mongodb.scala.model.Projections.{exclude, excludeId, fields}
+import org.slf4j.LoggerFactory
+
+import scala.io.Source
+
 
 object Main {
 
@@ -29,17 +39,19 @@ object Main {
     for (i <- 1 until pages.size - 1)
       listOfCars ++= getListOfCars(pages(i))
 
-    try {
-      for(i<-listOfCars.indices)
-      writeToFile(listOfCars(i).head.toString, parse(listOfCars(i)))
-    } catch {
-      case e: Exception => println("Error: %s".format(e.getMessage))
-    } finally {
-      println("Writing to JSON files finished!")
-    }
+//    try {
+//      for (i <- listOfCars.indices)
+//        writeToFile(listOfCars(i).head.toString, parse(listOfCars(i)))
+//    } catch {
+//      case e: Exception => println("Error: %s".format(e.getMessage))
+//    } finally {
+//      println("Writing to JSON files finished!")
+//    }
+
+    writeToDB()
+    queries()
 
   }
-
 
   private def getListOfCars(_page: String) = {
     val page = JsoupBrowser().get(_page)
@@ -79,10 +91,10 @@ object Main {
       val car = Car(
         preview.head.toString,
         preview(1).asInstanceOf[List[String]],
-        if (finalList.find(_.contains("Founded:")) != None) getFounded(finalList.find(_.contains("Founded:")).get) else "",
-        if (finalList.find(_.contains("Founder:")) != None) getFounder(finalList.find(_.contains("Founder: ")).get) else List.empty,
-        if (finalList.find(_.contains("Headquarters:")) != None) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else "",
-        if (finalList.find(_.contains("Official Site:")) != None) getSite(finalList.find(_.contains("Official Site:")).get) else ""
+        if (finalList.exists(_.contains("Founded:"))) getFounded(finalList.find(_.contains("Founded:")).get) else "",
+        if (finalList.exists(_.contains("Founder:"))) getFounder(finalList.find(_.contains("Founder: ")).get) else List.empty,
+        if (finalList.exists(_.contains("Headquarters:"))) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else "",
+        if (finalList.exists(_.contains("Official Site:"))) getSite(finalList.find(_.contains("Official Site:")).get) else ""
       )
 
       formJSON(car.name, car.spec, car.founded, car.founder, car.country, car.site)
@@ -107,10 +119,10 @@ object Main {
       val car = Car(
         preview.head.toString,
         preview(1).asInstanceOf[List[String]],
-        if (finalList.find(_.contains("Founded:")) != None) getFounded(finalList.find(_.contains("Founded:")).get) else "",
-        if (finalList.find(_.contains("Founder:")) != None) getFounder(finalList.find(_.contains("Founder: ")).get) else List.empty,
-        if (finalList.find(_.contains("Headquarters:")) != None) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else "",
-        if (finalList.find(_.contains("Official Site:")) != None) getSite(finalList.find(_.contains("Official Site:")).get) else ""
+        if (finalList.exists(_.contains("Founded:"))) getFounded(finalList.find(_.contains("Founded:")).get) else "",
+        if (finalList.exists(_.contains("Founder:"))) getFounder(finalList.find(_.contains("Founder: ")).get) else List.empty,
+        if (finalList.exists(_.contains("Headquarters:"))) getHeadquarters(finalList.find(_.contains("Headquarters:")).get) else "",
+        if (finalList.exists(_.contains("Official Site:"))) getSite(finalList.find(_.contains("Official Site:")).get) else ""
       )
 
       formJSON(car.name, car.spec, car.founded, car.founder, car.country, car.site)
@@ -118,7 +130,7 @@ object Main {
     }
   }
 
-  private def formJSON(name: String, spec: List[String], founded: String, founder: List[String], country: String, site: String) = prettyRender(
+  private def formJSON(name: String, spec: List[String], founded: String, founder: List[String], country: String, site: String) = compactRender(
     ("name" -> name) ~
       ("specialization" -> spec) ~
       ("founded" -> founded) ~
@@ -141,5 +153,91 @@ object Main {
   private def getHeadquarters(str: String) = "([\\w\\s.]+)$".r.findAllMatchIn(str).mkString("").substring(1)
 
   private def getSite(str: String) = str.substring(str.indexOf(":") + 2)
+
+  private def writeToDB(): Unit = {
+    val loggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+    val rootLogger = loggerContext.getLogger("org.mongodb.driver")
+    val rootLogger2 = loggerContext.getLogger("reactor.util.Loggers")
+    rootLogger.setLevel(Level.OFF)
+    rootLogger2.setLevel(Level.OFF)
+
+    val uri = "mongodb://localhost:27017"
+    val client: MongoClient = MongoClient(uri)
+    val db: MongoDatabase = client.getDatabase("cars")
+    val collection: MongoCollection[Document] = db.getCollection("brands")
+
+    val files = getListOfFiles("D:\\Uni\\NoSQL\\Cars\\JSON")
+    for (i <- files.indices) {
+      val tmp = Source.fromFile(files(i))
+      collection.insertOne(BsonDocument(tmp.mkString)).results()
+      tmp.close()
+    }
+    client.close()
+    println("Written to MongoDB!")
+  }
+
+  private def c() = {
+    val uri = "mongodb://localhost:27017"
+    val client: MongoClient = MongoClient(uri)
+    val db: MongoDatabase = client.getDatabase("cars")
+    val collection: MongoCollection[Document] = db.getCollection("brands")
+
+    println("\nЗапросы\n")
+
+    println("Вывести немецкие бренды производителей")
+    collection
+      .find(equal("country", "Germany"))
+      .limit(5)
+      .projection(fields(exclude("country"), excludeId()))
+      .printResults()
+    println("")
+
+    println("Вывести бренды, у которых больше одного создателя")
+    collection
+      .find(equal("founded", "1941"))
+      .projection(excludeId())
+      .printResults()
+    println("")
+
+    println("Вывести все бренды, созданные в 1941 год")
+    collection
+      .find(exists("founder.1", exists = true))
+      .projection(excludeId())
+      .sort(descending("name"))
+      .limit(4)
+      .printResults()
+    println("")
+
+    println("Вывести американские бренды, созданные до 1950")
+    collection.find(
+      or(
+        and(equal("country", "U.S."), lt("founded", "1950")),
+        and(equal("country", "United States"), lt("founded", "1950"))
+      ))
+      .projection(excludeId())
+      .limit(5)
+      .printResults()
+    println("")
+
+    println("Вывести все итальянские автомобили со специализацией «Роскошные автомобили», основанные до 1920 одним человеком")
+    collection.find(
+      or(
+        and(equal("specialization", "Luxury Vehicles"), equal("country", "Italy"), size("founder", 1), lte("founded", "1920")),
+        and(equal("specialization", "Luxury Cars"), equal("country", "Italy"), size("founder", 1), lte("founded", "1920"))
+      ))
+      .projection(excludeId())
+      .printResults()
+    println("")
+  }
+
+  private def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      d.listFiles.filter(_.isFile).toList
+    } else {
+      List[File]()
+    }
+  }
+
 }
 
